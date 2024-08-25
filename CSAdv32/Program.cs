6e7a126f6f95de86b2cb4ab1996becf6bf8c915a
jangjian <pureunnabi@gmail.com>
2024-08-25 22:35:45 +0900

    제너릭 실습 3

--- CSAdv32/Program.cs
+++ CSAdv32/Program.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace CSAdv32
 {
-
     class Wanted<T>  // Generic 
     {
         public T Value;
         public Wanted(T value)
         {
             Value = value;
+        }
+    }
+
+    class Needed<T, U> // Generic
+    {
+        public T Value1;
+        public U Value2;
+        public Needed(T value1, U value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
         }
     }
 
@@ -23,7 +32,6 @@ namespace CSAdv32
             Wanted<int> wantedInt = new Wanted<int>(65535);
             Wanted<string> wantedString = new Wanted<string>("Hello, World!");
             Wanted<double> wantedDouble = new Wanted<double>(3.14159);
-
             Console.WriteLine(wantedInt.Value);
             Console.WriteLine(wantedString.Value);
             Console.WriteLine(wantedDouble.Value);
