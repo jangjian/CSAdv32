34056daaecd36ee5bf8998d8e0d46fb5034fded7
jangjian <pureunnabi@gmail.com>
2024-08-25 22:42:12 +0900

    구조체 실습2 - 생성자

--- CSAdv32/Program.cs
+++ CSAdv32/Program.cs
@@ -44,37 +44,49 @@ namespace CSAdv32
             get { return i * i; }
         }
     }
-
     internal class Program
     {
         struct Point
         {
             public int x;
             public int y;
+            public string testA;
+            public string testB;
+            //public string testB = "init"; // 구조체 내 변수는 선언과 동시에 초기화 불가능
+
+            //public Point() { }  //구조체 -> 기본생성자 정의 불가능
+
+            public Point(int x, int y) // 구조체 생성자 -> 모든 변수 초기화 해주어야 함
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = "init";
+                this.testB = "init";
+            }
+            public Point(int x, int y, string t) // 생성자 오버로딩 가능
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = t;
+                this.testB = t;
+            }
         }
 
         static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+
+
+
+
+
+
+
+
+          Expand Down
+
+
+
         {
             rx = x + vx;
-
-
-
-
-
-
-
-            Expand Down
-
-
-
-
-
-
-          Expand Up
-    
-    @@ -92,6 + 98,12 @@ static void Main(string[] args)
-
-
             ry = y + vy;
         }
         static void Main(string[] args)
@@ -105,7 +117,6 @@ namespace CSAdv32
             Console.WriteLine("현재좌표 x: {0}, y: {1}", x, y);
             NextPos(x, y, vx, vy, out x, out y);
             Console.WriteLine("다음좌표 x: {0}, y: {1}", x, y);
-
             // Point 구조체 실습
             Point point;   // 구조체 변수 선언. 8바이트 잡힘.
             point.x = 10;  // 구조체 변수는 반드시 초기화 해야함.(컴파일 에러)
